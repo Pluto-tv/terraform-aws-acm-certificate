619dabd27d9971df1747b5f76abef97e2e8936a4
Iggy Jackson <iggy.jackson@pluto.tv>
2019-12-09 12:29:04 -0800

    Use count lookups now that everything has a count

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ resource "aws_acm_certificate" "default" {
 resource "aws_route53_record" "validation" {
   provider        = aws.route53_account
   count           = var.create_cert == true ? length(var.subject_alternative_names) + 1 : 0
-  name            = aws_acm_certificate.default.domain_validation_options[count.index]["resource_record_name"]
-  type            = aws_acm_certificate.default.domain_validation_options[count.index]["resource_record_type"]
+  name            = aws_acm_certificate.default.0.domain_validation_options[count.index]["resource_record_name"]
+  type            = aws_acm_certificate.default.0.domain_validation_options[count.index]["resource_record_type"]
   zone_id         = var.hosted_zone_id
-  records         = [aws_acm_certificate.default.domain_validation_options[count.index]["resource_record_value"]]
+  records         = [aws_acm_certificate.default.0.domain_validation_options[count.index]["resource_record_value"]]
   ttl             = var.validation_record_ttl
   allow_overwrite = var.allow_validation_record_overwrite
 }
@@ -26,6 +26,6 @@ resource "aws_route53_record" "validation" {
 resource "aws_acm_certificate_validation" "default" {
   count                   = var.create_cert == true ? 1 : 0
   provider                = aws.acm_account
-  certificate_arn         = aws_acm_certificate.default.arn
+  certificate_arn         = aws_acm_certificate.default.0.arn
   validation_record_fqdns = aws_route53_record.validation.*.fqdn
 }
