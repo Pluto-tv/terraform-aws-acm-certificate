8bde6a16bc0c419b430f7099e6265c1525fa0f8f
Iggy Jackson <iggy.jackson@pluto.tv>
2019-12-09 12:43:12 -0800

    Use count lookups now that everything has a count

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "arn" {
-  value = aws_acm_certificate_validation.default.0.certificate_arn
+  value = aws_acm_certificate_validation.default == [] ? "none" : aws_acm_certificate_validation.default.0.certificate_arn
 }
